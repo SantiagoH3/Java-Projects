91ba16723784c8263249f5db63aa563a76e1b90d
SantiagoH3 <35846982+SantiagoH3@users.noreply.github.com>
2019-04-03 19:51:27 -0700

    My first App Project.

---
+++ MyFirstApp/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Esta es la tabla del 12 \n");
+        for(int i = 0; i <= 12; i++)
+        {
+            System.out.println("12 * " + i + " = " + 12 * i + "\n");
+        }
+    }
+}
